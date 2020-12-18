6d979938873cffca10be0f34a9871b0d13144f13
novemberizing <novemberizing@gmail.com>
2020-12-18 12:01:20 +0900

    update

--- core/include/novemberizing/zip.h
+++ core/include/novemberizing/zip.h
@@ -1,7 +1,15 @@
 #ifndef   __NOVEMBERIZING_ZIP__H__
 #define   __NOVEMBERIZING_ZIP__H__
 
-extern "C" int unzip(const char * path);
-extern "C" int zip(const char * path);
+#ifdef __cplusplus
+extern "C" {
+#endif // __cplusplus
+
+int unzip(const char * path);
+int zip(const char * path);
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
 
 #endif // __NOVEMBERIZING_ZIP__H__
